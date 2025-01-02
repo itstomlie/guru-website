11f79429f3fb547bd309e3c4a1146697c6f4f61d
Tommy <itsTomLie@gmail.com>
2025-01-03 00:11:28 +0800

    added deep link

--- app/app/[...slug]/page.tsx
+++ app/app/[...slug]/page.tsx
@@ -11,7 +11,7 @@ const DynamicPage = () => {
   useEffect(() => {
     const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
     if (isMobile && slug) {
-      const deepLink = `myapp://${slug}?token=${token}`
+      const deepLink = `guruu://${slug}?token=${token}`
       window.location.href = deepLink
     }
   }, [slug, token])
